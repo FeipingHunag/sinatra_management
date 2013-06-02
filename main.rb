beb02307c6b28c47b3c4c80b3b3ec0f14b25031d
feipinghuang <huang900107@163.com>
2013-06-02 16:05:56 +0800

    part two

--- main.rb
+++ main.rb
@@ -11,8 +11,12 @@ class Page
   include Mongoid::Document
   field :title, type: String
   field :content, type: String
-end
+  field :permalink, type: String, default: -> { make_permalink }
 
+  def make_permalink
+    title.downcase.gsub(/\W/,'-').squeeze('-').chomp('-') if title
+  end
+end
 
 get '/pages' do
   @pages = Page.all
@@ -31,12 +35,42 @@ get '/pages/new' do
   slim :new
 end
 
-
-
 get '/pages/:id' do
   @page = Page.find(params[:id])
   @title = @page.title
   slim :show
 end
+
+get '/pages/:id/edit' do
+  @page = Page.find(params[:id])
+  slim :edit
+end
+
+put '/pages/:id' do
+  page = Page.find(params[:id])
+  page.update_attributes(params[:page])
+  redirect to("/pages/#{page.id}")
+end
+
+get '/pages/delete/:id' do
+  @page = Page.find(params[:id])
+  slim :delete
+end
+
+delete '/pages/:id' do
+  Page.find(params[:id]).destroy
+  redirect to('/pages')
+end
+
+get '/:permalink' do
+  begin
+    @page = Page.find_by(permalink: params[:permalink])
+  rescue
+    pass
+  end
+  slim :show
+end
+
+get('/styles/main.css'){ scss :styles }
 
 
